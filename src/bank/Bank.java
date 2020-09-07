bf6b0aaaf24e401555f893f3c7606c9759e883a6
TaRoQa <69321241+TaRoQa@users.noreply.github.com>
2020-09-07 16:14:20 +0200

    update project

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javax.swing.JOptionPane;
 
 /**
@@ -23,7 +24,7 @@ public class Bank extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
         Scene scene = new Scene(root);
-        
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
